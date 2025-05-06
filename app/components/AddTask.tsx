51180a8312f582f0088f8c54dce603753aa1669e
Artiom Sviridovich <jsat1987@gmail.com>
2025-05-06 11:36:45 +0200

    feature-2: create modal window

--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -1,12 +1,24 @@
+'use client'
+
+import { useState } from 'react'
 import { AiOutlinePlus } from 'react-icons/ai'
+import Modal from './Modal'
 
 const AddTask = () => {
+  const [modalOpen, setModalOpen] = useState<boolean>(false)
+
   return (
     <div>
-      <button className="btn btn-primary w-full">
+      <button
+        onClick={() => setModalOpen(true)}
+        className="btn btn-primary w-full"
+      >
         Add new task
-        <AiOutlinePlus className='ml-2' size={18} />
+        <AiOutlinePlus className="ml-2" size={18} />
       </button>
+      <Modal modalOpen={modalOpen} setModalOpen={setModalOpen}>
+        Modal for add todo
+      </Modal>
     </div>
   )
 }
